e72883dc92113a02a35624c065299f4835e40a9b
zhangya <905290841@qq.com>
2014-12-25 18:18:14 +0800

    更新 优化fclass分类，

---
+++ src/com/youai/aistore/Fclass/MyGridView.java
@@ -0,0 +1,36 @@
+package com.youai.aistore.Fclass;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * @Type: MyGridView 
+ * 让GridView可以做ScrollView的子控件，但尺寸不会减小 
+ */
+public class MyGridView extends GridView {
+
+	public MyGridView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// TODO Auto-generated method stub
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,    
+                MeasureSpec.AT_MOST);    
+		super.onMeasure(widthMeasureSpec, expandSpec);
+	}
+
+}
